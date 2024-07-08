b63f41384f12c1d1bea055d0cc69cf3599d10ddf
plutuss <dima92konovalenko@gmail.com>
2024-07-08 10:09:34 +0200

    update: config

--- src/config/geo-names.php
+++ src/config/geo-names.php
@@ -1,5 +1,9 @@
 <?php
 
+use Plutuss\GeoNames\Response\Adapter\ArrayReportAdapter;
+use Plutuss\GeoNames\Response\Adapter\CollectionReportAdapter;
+use Plutuss\GeoNames\Response\Adapter\JsonReportAdapter;
+
 return [
 
     /*
@@ -10,6 +14,32 @@ return [
    */
     'username' => env('GEO_NAMES_USERNAME', ''),
     'endpoint' => env('GEO_NAMES_ENDPOINT', 'http://api.geonames.org'),
-    'country-code' => '' // 'DE','US','FR',
+    'country-code' => '', // 'DE','US','FR',
+
+
+    'type_response' => [
+
+        /**
+         *  Arrays with wrapper with class Answer
+         */
+        'arrays_with_wrapper' => false,
+
+        /**
+         * You can choose the format of your answer
+         *
+         * collection, array, json
+         */
+        'default' => env('GEO_NAMES_RESPONSE', 'collection'),
+
+        /**
+         * You can change the response class by using ReportAdapterInterface in your class
+         */
+        'response_class' => [
+            'collection' => CollectionReportAdapter::class,
+            'array' => ArrayReportAdapter::class,
+            'json' => JsonReportAdapter::class,
+        ],
+
+    ],
 
 ];
